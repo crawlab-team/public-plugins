d388577840ea7bd471f8aa70fad68277b799623f
Marvin Zhang <tikazyq@163.com>
2022-06-30 20:42:52 +0800

    fix: get all plugins from github api issue

--- scripts/getPlugins.js
+++ scripts/getPlugins.js
@@ -10,7 +10,7 @@ const GITHUB_PUBLIC_ORG = 'crawlab-team'
 async function main() {
   const url = `https://api.github.com/orgs/${GITHUB_PUBLIC_ORG}/repos`
   const res = await axios.get(url)
-  const pluginsData = res.data?.filter(d => d.name.match(/^crawlab-plugin-/))
+  const pluginsData = res.data.filter(d => d.name.match(/^crawlab-plugin-/))
   const jsonData = JSON.stringify(pluginsData)
   const filePath = 'plugins.json'
   fs.writeFileSync(filePath, jsonData)
