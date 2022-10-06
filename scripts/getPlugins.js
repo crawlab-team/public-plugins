cce877da2af6815a4a21aa8bb2130218d5157759
Marvin Zhang <tikazyq@163.com>
2022-10-06 10:32:49 +0800

    chore: escape dependency and notification

--- scripts/getPlugins.js
+++ scripts/getPlugins.js
@@ -7,10 +7,15 @@ const {
 
 const GITHUB_PUBLIC_ORG = 'crawlab-team'
 
+const escaped = [
+  /dependency/,
+  /notification/,
+]
+
 async function main() {
   const url = `https://api.github.com/orgs/${GITHUB_PUBLIC_ORG}/repos`
   const res = await axios.get(url)
-  const pluginsData = res.data.filter(d => d.name.match(/^plugin-/))
+  const pluginsData = res.data.filter(d => d.name.match(/^plugin-/) && !escaped.some(e => e.test(d.name)))
   const jsonData = JSON.stringify(pluginsData)
   const filePath = 'plugins.json'
   fs.writeFileSync(filePath, jsonData)
